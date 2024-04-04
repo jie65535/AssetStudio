71b4e4888b12244c834ccf167650e442cac96b1d
jie65535 <jie65535@qq.com>
2024-04-04 13:58:36 +0800

    Update Exporter.cs

--- AssetStudioCLI/Exporter.cs
+++ AssetStudioCLI/Exporter.cs
@@ -350,12 +350,9 @@ namespace AssetStudioCLI
                     break;
             }
             fullPath = Path.Combine(dir, fileName + extension);
+            if (!File.Exists(fullPath))
+                Directory.CreateDirectory(dir);
             return true; // 如果文件已经存在，直接覆盖
-            // if (!File.Exists(fullPath))
-            // {
-            //     Directory.CreateDirectory(dir);
-            //     return true;
-            // }
             // if (filenameFormat == FilenameFormat.AssetName)
             // {
             //     fullPath = Path.Combine(dir, fileName + item.UniqueID + extension);
